328556d88e2aba04455d58e1d8733c84dbd5f6b0
Rose Maina <rosewairimu27@gmail.com>
2022-10-13 10:46:36 +0300

    Updated the MVC

---
+++ db/schema.rb
@@ -0,0 +1,20 @@
+ActiveRecord::Schema.define(version: 2022_10_13_070047) do
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "title"
+    t.text "instructions"
+    t.integer "minutes_to_complete"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.string "image_url"
+    t.string "bio"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Seeding data."
+Recipe.create(
+    title: "Ugali", 
+    instructions: "boil water, add unga and knead till its well done", 
+    minutes_to_complete: 10, 
+    user_id: 1
+)
+
+puts "Done Seeding"
